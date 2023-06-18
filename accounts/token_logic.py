c743c31ec906e463a2e6e5dffe49ddd75bcf7839
Houdini888 <56999380+Houdini888@users.noreply.github.com>
2023-06-18 03:38:38 +0200

    Added token logic for authorization of JWT tokens

---
+++ accounts/token_logic.py
@@ -0,0 +1,28 @@
+import jwt
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from rest_framework.exceptions import AuthenticationFailed
+
+User = get_user_model()
+
+def get_token_from_header(request):
+    try:
+        auth_header = request.headers.get('Authorization')
+        if auth_header and auth_header.startswith('Bearer '):
+            token = auth_header.split(' ')[1]
+            return token
+        else:
+            raise AuthenticationFailed('Invalid token format')
+    except Exception as e:
+        raise AuthenticationFailed('Failed to retrieve token from header') from e
+
+
+def get_user_from_token(token) -> User:
+    try:
+        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        user_id = decoded_token['user_id']
+        # Retrieve the user object using the user_id
+        user = User.objects.get(id=user_id)
+        return user
+    except (jwt.DecodeError, User.DoesNotExist):
+        return None
