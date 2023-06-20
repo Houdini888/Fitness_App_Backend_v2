d67660405e32205d33022e1e97399b75246cb7f4
lukcyn <lukaszcyndzerr@gmail.com>
2023-06-20 20:21:49 +0200

    Url refactoring again

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('registration/', views.RegistrationView.as_view(), name='registration'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('user-data/', views.GetUserData.as_view(), name='get_user_data'),
-    path('password/', views.ChangePasswordView.as_view(), name='change_password')
+    path('password-change/', views.ChangePasswordView.as_view(), name='change_password')
 ]
