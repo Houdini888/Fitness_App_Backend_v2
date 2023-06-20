e1751d8e85b074c4a1fe3a91a0b8f2c31724f819
lukcyn <lukaszcyndzerr@gmail.com>
2023-06-20 19:26:09 +0200

    Url refactoring

--- meals/urls.py
+++ meals/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_meal/', views.AddMealView.as_view(), name='add_meal'),
-    path('add_product_to_meal/', views.AddProductToMeal.as_view(), name='add_product_to_meal'),
-    path('remove_product_from_meal/', views.RemoveProductFromMeal.as_view(), name='remove_product_from_meal'),
-
+    path('meal/', views.AddMealView.as_view(), name='add_meal'),
+    path('add-product/', views.AddProductToMeal.as_view(), name='add_product_to_meal'),
+    path('remove-product/', views.RemoveProductFromMeal.as_view(), name='remove_product_from_meal'),
 ]
--- meals/views.py
+++ meals/views.py
@@ -11,54 +11,53 @@ from datetime import date
 import json
 
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
-#add a meal with all initial products
+
+# add a meal with all initial products
 class AddMealView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        user = authenticate_user_from_request(request)
 
+        if user:
+            try:
+                meal = Meal.objects.create(
+                    title=request.data.get('title'),
+                    subtitle=request.data.get('subtitle'),
+                    calories=request.data.get('calories'),
+                    creation_date=date.today(),
+                    creator_user=user,
+                )
+            except:
+                return Response({'error': 'Invalid meal data'}, status=400)
 
-            user = authenticate_user_from_request(request)
+            # products_id_quant_list is a list of dicts with keys {product_id, quantity}
+            products_id_quant_list = request.data.get('products')
 
-            if user:
-                try:
+            for product_id_quant in products_id_quant_list:
+                prod_id = product_id_quant['product_id']
+                product = Product.objects.get(id=prod_id)
 
-                    meal = Meal.objects.create(
-                        title=request.data.get('title'),
-                        subtitle=request.data.get('subtitle'),
-                        calories=request.data.get('calories'),
-                        creation_date=date.today(),
-                        creator_user=user,
-
+                if product:
+                    MealElement.objects.create(
+                        product=product,
+                        meal=meal,
+                        quantity=product_id_quant['quantity']
                     )
-                except:
-                    return Response({'error': 'Invalid meal data'}, status=400)
+                else:
+                    return Response({'error': f'Invalid product id: {prod_id}'}, status=400)
+
+        else:
+            return Response({'error': 'Invalid token'}, status=400)
+
+        return Response({'message': 'Meal added successfully', 'meal_id': meal.id})
 
 
-                #products_id_quant_list is a list of dicts with keys {product_id, quantity}
-                products_id_quant_list = request.data.get('products')
-
-                for product_id_quant in products_id_quant_list:
-                    product = Product.objects.get(id=product_id_quant['product_id'])
-
-                    if product:
-                        MealElement.objects.create(
-                            product=product,
-                            meal=meal,
-                            quantity=product_id_quant['quantity']
-                        )
-                    else:
-                        return Response({'error': 'Invalid product'}, status=400)
-
-            else:
-                return Response({'error': 'User authentication failed'}, status=400)
-
-            return Response({'message': 'Meal added successfully', 'meal_id': meal.id})
-
-#get request with meal_id, product_id and quantity and add product to meal's product_list
+# get request with meal_id, product_id and quantity and add product to meal's product_list
 class AddProductToMeal(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -68,7 +67,6 @@ class AddProductToMeal(APIView):
 
         if user:
             try:
-
                 meal_id = request.data.get('meal_id')
                 product_id = request.data.get('product_id')
                 quantity = request.data.get('quantity')
@@ -82,7 +80,8 @@ class AddProductToMeal(APIView):
 
             return Response({'message': 'Product successfully added to meal'})
 
-#get request with meal_id and product_id, then remove product from meal
+
+# get request with meal_id and product_id, then remove product from meal
 class RemoveProductFromMeal(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -104,5 +103,3 @@ class RemoveProductFromMeal(APIView):
                 return Response({'error': 'Invalid meal or product data'}, status=400)
 
         return Response({'message': 'Product successfully removed from meal'})
-
-
