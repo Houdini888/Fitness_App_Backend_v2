9ac7cdb31568161edd8af2f01b0866c621114991
lukcyn <77535983+lukcyn@users.noreply.github.com>
2023-06-20 19:32:56 +0200

    url update

--- meals/urls.py
+++ meals/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('meal/', views.AddMealView.as_view(), name='add_meal'),
     path('add-product/', views.AddProductToMeal.as_view(), name='add_product_to_meal'),
     path('remove-product/', views.RemoveProductFromMeal.as_view(), name='remove_product_from_meal'),
-    path('with-title/', views.GetProductsByTitle.as_view(), name='get_products_by_title'),
+    path('product-with-title/', views.GetProductsByTitle.as_view(), name='get_products_by_title'),
     path('product/', views.GetProduct.as_view(), name='product'),
 ]
