565fd46b703b0b4697b21bdc27983a8a0049a052
Houdini888 <56999380+Houdini888@users.noreply.github.com>
2023-06-20 21:34:29 +0200

    Product title in json quickfix

--- meals/serializers.py
+++ meals/serializers.py
@@ -5,4 +5,4 @@ from .models import Product, Meal, MealElement
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'measure_type', 'kcal', 'isVerified']
+        fields = ['id', 'title', 'measure_type', 'kcal', 'isVerified']
